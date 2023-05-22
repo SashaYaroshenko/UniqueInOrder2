c4657a3d11506f779f25b82d1784f9ddf83f1c50
SashaYaroshenko <yaroshenko2019ol@gmail.com>
2023-05-22 17:24:37 +0300

    Add VS project

---
+++ LibraryUniqueInOrd/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+public static class Kata
+{
+    public static IEnumerable<T> UniqueInOrder<T>(IEnumerable<T> iterable)
+    {
+
+        // if(iterable.GetType() == typeof(string))
+        if (iterable is string)
+        {
+            List<char> list = (iterable as string).ToList();
+            List<char> result = new List<char>();
+
+
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                if (list[i] != list[i + 1])
+                {
+                    result.Add(list[i]);
+                }
+
+            }
+            return result;
+        }
+        return result;
+    }
+}
---
+++ TestUniqueInOrd/UnitTest1.cs
@@ -0,0 +1,20 @@
+namespace Solution
+{
+    using NUnit.Framework;
+    using System;
+
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        public void EmptyTest()
+        {
+            Assert.AreEqual("", Kata.UniqueInOrder(""));
+        }
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual("ABCDAB", Kata.UniqueInOrder("AAAABBBCCDAABBB"));
+        }
+    }
+}
